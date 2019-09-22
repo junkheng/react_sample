e2a7dd49dc32a7a9e15b5b9849c717e7d1bcfbc8
Jun Kheng <junkheng@gmail.com>
2019-09-22 15:25:20 +0800

    will be back

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Login from './Login'
-import Footer from './Footer'
-import logo from './img/logo.png';
+// import Login from './Login'
+// import Footer from './Footer'
+import logo from './img/the-terminator.jpg';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Dashboard from './Dashboard';
+// import Dashboard from './Dashboard';
 import {Grid} from '@material-ui/core';
-import Learning from './Learning';
-import Mentor from './Mentor'
-import Mentor2 from './Mentor2'
-import Mentor3 from './Mentor3'
+// import Learning from './Learning';
+// import Mentor from './Mentor'
+// import Mentor2 from './Mentor2'
+// import Mentor3 from './Mentor3'
 
 function App() {
 	return (
@@ -23,27 +23,27 @@ function App() {
                                     <img src={logo} className="App-logo" alt="logo" />
                                     <Grid className="AppTextContainer" item xs={12}>
                                         <h3 className="App-text">
-                                            Welcome Back
+                                            Sneaky.app will be back!
                                         </h3>
                                     </Grid>
-                                    <Grid className="AppTextContainer" item xs={12}>
+                                    {/* <Grid className="AppTextContainer" item xs={12}>
                                         <h6 className="App-text">
                                             Sign in to continue
                                         </h6>
-                                    </Grid>
-                                    <Login />
+                                    </Grid> */}
+                                    {/* <Login /> */}
                                 </header>
-                                <Footer />
+                                {/* <Footer /> */}
                             </div>
                         </Grid>
                     </Grid>
 				)
 			}} />
-            <Route exact path="/Mentor" component={Mentor} />
+            {/* <Route exact path="/Mentor" component={Mentor} />
             <Route exact path="/Mentor2" component={Mentor2} />
             <Route exact path="/Mentor3" component={Mentor3} />
             <Route exact path="/Learning" component={Learning} />
-			<Route exact path="/Dashboard" component={Dashboard} />
+			<Route exact path="/Dashboard" component={Dashboard} /> */}
 		</Router>
 	);
 }
